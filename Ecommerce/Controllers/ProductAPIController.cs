e865071f4fd550b7a123a88d7395d359213e376f
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-04-03 23:44:07 +0600

    till coding

---
+++ Ecommerce/Controllers/ProductAPIController.cs
@@ -0,0 +1,36 @@
+﻿using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class ProductAPIController : Controller
+    {
+        public IActionResult LisProduct()
+        {
+            List<Product> lstPoducts = new List<Product>();
+            Product product = new Product();
+            product.Name = "Laptop 1";
+            product.Picture = "https://media.istockphoto.com/id/1266659893/vector/blank-white-screen-laptop-isolated.jpg?s=612x612&w=0&k=20&c=rrRWxOtMQ6NBLg1tgNJPhdLNJkiAgGPrJhruyuKclYc=";
+            product.Price = 100;
+            product.Quantity = 10;
+            lstPoducts.Add(product);
+
+            product = new Product();
+            product.Name = "Laptop 2";
+            product.Picture = "https://media.istockphoto.com/id/1266659893/vector/blank-white-screen-laptop-isolated.jpg?s=612x612&w=0&k=20&c=rrRWxOtMQ6NBLg1tgNJPhdLNJkiAgGPrJhruyuKclYc=";
+            product.Price = 100;
+            product.Quantity = 10;
+            lstPoducts.Add(product);
+
+            product = new Product();
+            product.Name = "Laptop 3";
+            product.Picture = "https://media.istockphoto.com/id/1266659893/vector/blank-white-screen-laptop-isolated.jpg?s=612x612&w=0&k=20&c=rrRWxOtMQ6NBLg1tgNJPhdLNJkiAgGPrJhruyuKclYc=";
+            product.Price = 100;
+            product.Quantity = 10;
+            lstPoducts.Add(product);
+
+
+            return Ok(lstPoducts);
+        }
+    }
+}
---
+++ Ecommerce/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace Ecommerce.Models
+{
+    public class Product
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Picture {get; set;}
+        [DataMember]
+        public int Price {  get; set;}
+        [DataMember]
+        public int Quantity {  get; set;}
+    }
+}
