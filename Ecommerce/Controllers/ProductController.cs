40a8d54f6964a2cae054b39ac3adcdd9e967a080
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-04-03 23:09:44 +0600

    Add project files.

---
+++ Ecommerce/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult ProductName()
+        {
+            return Ok("Product Name API Success");
+        }
+    }
+}
